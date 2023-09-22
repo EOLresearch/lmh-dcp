abb82b04daeff3925f5a3cee7881c7f4b0b47665
mrjoemiller <joe@joemiller.co>
2023-09-22 17:34:02 -0400

    login and reg conditional render

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Home from './views/Home';
 import Dashboard from './views/Dashboard';
 import Login from './views/Login';
 
+import door from '../src/assets/img/door-md.png';
+
 function App() {
-  const [entered, setEntered] = useState(false);
+  const [entered, setEntered] = useState(true);
 
   function handleClick(event) {
     console.log("Type of element:", event.target.tagName);
@@ -19,14 +21,16 @@ function App() {
     return (
       <div className='doorstep-container'>
         <div onClick={handleClick} className='doorstep'></div>
-        <div onClick={handleClick} className='door'></div>
+        <div onClick={handleClick} className='door'>
+          <img src={door} alt="Door" />
+        </div>
       </div>
     )
   }
 
   return (
     <Router>
-      <nav>
+      {/* <nav>
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -38,7 +42,7 @@ function App() {
             <Link to="/login">Login</Link>
           </li>
         </ul>
-      </nav>
+      </nav> */}
 
       <Routes>
         <Route path="/login" element={<Login />} />
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -4,40 +4,103 @@ import React, { useState } from 'react';
 import './login.css';
 
 function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [screen, setScreen] = useState("login");  // Values can be "login" or "register"
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // Implement your login logic here, e.g., API call, validation, etc.
-  };
+  const [email, setEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [givenName, setGivenName] = useState("");
+  const [firstNameDeceased, setFirstNameDeceased] = useState("");
+  const [redCapID, setRedCapID] = useState("");
+
+  const handleRegistration = () => {
+    console.log("Email:", email);
+  }
 
   return (
-    <div className="login">
-      <h1>Login to Memory Home</h1>
-      <form onSubmit={handleLogin}>
-        <div className="input-group">
-          <label htmlFor="username">Username:</label>
-          <input 
-            type="text" 
-            id="username" 
-            value={username} 
-            onChange={e => setUsername(e.target.value)} 
-          />
+    <div className="login-container">
+        <header className="header">
+            <img src="/path/to/logo.png" alt="Logo" className="logo" />
+            {/* Add other header items if necessary */}
+        </header>
+        <div className="content">
+            <div className="image-container">
+                <img src="/path/to/image.png" alt="Image Description" />
+            </div>
+            {screen === "login" ? (
+                <div className="form-container">
+                    <h2>Login</h2>
+                    <input 
+                        type="text" 
+                        id="username" 
+                        placeholder="Username"
+                        value={username} 
+                        onChange={e => setUsername(e.target.value)} 
+                    />
+                    <input 
+                        type="password" 
+                        id="password" 
+                        placeholder="Password"
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)} 
+                    />
+                    <button>Login</button>
+                    <button onClick={() => setScreen("register")} className="switch-to-register">
+                        No account? Create one now
+                    </button>
+                </div>
+            ) : (
+                <div className="form-container">
+                    <h2>Register</h2>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        placeholder="Email*" 
+                        onChange={e => setEmail(e.target.value)} 
+                    />
+                    <input 
+                        type="password" 
+                        id="password" 
+                        placeholder="Password*" 
+                        onChange={e => setRegisterPassword(e.target.value)} 
+                    />
+                    <input 
+                        type="password" 
+                        id="confirmPassword" 
+                        placeholder="Please confirm your password*" 
+                        onChange={e => setConfirmPassword(e.target.value)} 
+                    />
+                    <input 
+                        type="text" 
+                        id="givenName" 
+                        placeholder="Your Given Name" 
+                        onChange={e => setGivenName(e.target.value)} 
+                    />
+                    <input 
+                        type="text" 
+                        id="firstNameDeceased" 
+                        placeholder="First Name of the Deceased*" 
+                        onChange={e => setFirstNameDeceased(e.target.value)} 
+                    />
+                    <input 
+                        type="text" 
+                        id="redCapID" 
+                        placeholder="REDCap ID (if you have one)" 
+                        onChange={e => setRedCapID(e.target.value)} 
+                    />
+                    <button onClick={handleRegistration}>Register</button>
+                    <button onClick={() => setScreen("login")} className="switch-to-login">
+                        Already have an account? Log in
+                    </button>
+                </div>
+            )}
         </div>
-        <div className="input-group">
-          <label htmlFor="password">Password:</label>
-          <input 
-            type="password" 
-            id="password" 
-            value={password} 
-            onChange={e => setPassword(e.target.value)} 
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+        <footer className="footer">
+            © 2023 Your Company Name. All rights reserved.
+        </footer>
     </div>
-  );
-}
-
+);
+};
 export default Login;
