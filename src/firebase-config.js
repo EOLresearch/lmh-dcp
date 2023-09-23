8839d51e0eb1d13745b73ecc2de12aa3b8b24a3a
mrjoemiller <joe@joemiller.co>
2023-09-23 09:55:53 -0400

    firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZZccWpd7VkLnq1-3uxfabYmxBLXTCH7I",
+  authDomain: "lmh-dcp.firebaseapp.com",
+  projectId: "lmh-dcp",
+  storageBucket: "lmh-dcp.appspot.com",
+  messagingSenderId: "328488718801",
+  appId: "1:328488718801:web:46e75e6e58a18da29c90f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
