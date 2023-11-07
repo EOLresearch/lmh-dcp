af4d2cf3b18cf2bfe843b0b2a12e81d0e2daba9b
mrjoemiller <joe@joemiller.co>
2023-11-07 10:19:09 -0500

    cleanup from distraction

--- src/components/Mementos/index.js
+++ src/components/Mementos/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './mementos.css';
+import openbook from '../../assets/img/openbook.png';
 
 function Mementos() {
   const [currentPrompt, setCurrentPrompt] = useState(0);
@@ -19,13 +20,19 @@ function Mementos() {
 
   return (
     <div className="mementos-container">
+      <img src={openbook} alt="Mementos" />
       <div className="mementos-content">
-        <div className="intro-text">
-          <h2>Welcome to Mementos</h2>
+        <div className="left-side">
+          <h2>Welcome to your Mementos</h2>
           <p>
-            Mementos is a place where you can share your life stories and memories. We'll guide you through a narrative exercise to create a meaningful record of your experiences.
+            This book is a place where you can share your life stories and memories. We'll guide you through a narrative exercise to create a meaningful record of your experiences.
           </p>
         </div>
+        <div className="right-side">
+          <h3>Today's Prompt</h3>
+          <p>{prompts[currentPrompt]}</p>
+          <button onClick={handleNextPrompt}>Next</button>
+          </div>
       </div>
     </div>
   );
