26ad5e5a3cb55aa26277ba25bdad4e713d9af63f
mrjoemiller <joe@joemiller.co>
2024-05-03 13:24:53 -0400

    memory lane work

--- src/views/ReminiscenceRoom/index.js
+++ src/views/ReminiscenceRoom/index.js
@@ -7,24 +7,34 @@ import { useAuth } from "../../auth/AuthContext";
 import fame from '../../assets/img/fame.png'
 import memory from '../../assets/img/memory.png'
 
+import dogplayingstock from '../../assets/img/stockphotos/dogplayingstock.png'
+import familypicnicstock from '../../assets/img/stockphotos/familypicnicstock.png'
+import mountainstock from '../../assets/img/stockphotos/mountainstock.png'
+import parisstock from '../../assets/img/stockphotos/parisstock.png'
+
+const photoAlbum = [
+  { id: 1, src: dogplayingstock, caption: 'Dog Playing' },
+  { id: 2, src: familypicnicstock, caption: 'Family Picnic' },
+  { id: 3, src: mountainstock, caption: 'Mountain' },
+  { id: 4, src: parisstock, caption: 'Paris' },
+];
+
+
 function ReminiscenceRoom() {
   const { userData } = useAuth();
   const { houseSelection } = userData;
 
+  const [showLifebook, setShowLifebook] = useState(false);
+
   const roomStyle = {
     backgroundImage: `url(${houseSelection.images.rem})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: 'calc(100vh - 100px)',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
   };
 
+
   return (
-    <div style={roomStyle}>
+    <div style={roomStyle} className='rem-room-container'>
       <div className="room-menu">
-        <button className="menu-btn" >
+        <button className="menu-btn" onClick={() => setShowLifebook(true)}>
           <img src={memory} alt="Writing Down Memory Lane" />
           <span>Writing Down Memory Lane</span>
         </button>
@@ -33,6 +43,23 @@ function ReminiscenceRoom() {
           <span>Wall of Fame</span>
         </button>
       </div>
+      {showLifebook && <div className="overlay" />}
+      {showLifebook &&
+        <div className="lifebook">
+          <div className="lifebook-content">
+            <button className="close-btn" onClick={() => setShowLifebook(false)}>X</button>
+            <div className="photo-album">
+              {photoAlbum.slice(0, 2).map(photo => (
+                <div className='photo' key={photo.id}>
+                  <img key={photo.id} src={photo.src} alt={photo.caption} />
+                  <p>{photo.caption}</p>
+                </div>
+              ))}
+            </div>
+
+          </div>
+        </div>
+      }
     </div>
   );
 }
