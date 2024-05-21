18118eeaf88fc539c00f3bb33b12743f395c0c8e
mrjoemiller <joe@joemiller.co>
2024-05-20 20:25:41 -0400

    photo upload work

--- src/views/ReminiscenceRoom/MemoryLane.js
+++ src/views/ReminiscenceRoom/MemoryLane.js
@@ -3,29 +3,52 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 import { FaLongArrowAltLeft } from "react-icons/fa";
 import { BsXLg } from "react-icons/bs";
 
-const MemoryLane = ({ setShowMemoryLane, handlePhotoClick, photoAlbum, }) => {
+const MemoryLane = ({ setShowMemoryLane, handlePhotoClick, handlePhotoUpload, photoAlbum }) => {
   const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+
   const nextPage = () => {
     setCurrentPhotoIndex((prevIndex) => {
       const newIndex = prevIndex + 2;
-      return newIndex >= photoAlbum.length ? newIndex - photoAlbum.length : newIndex;
+      return newIndex >= photoAlbum.length ? 0 : newIndex;
     });
   };
 
   const prevPage = () => {
     setCurrentPhotoIndex((prevIndex) => {
       const newIndex = prevIndex - 2;
-      return newIndex < 0 ? newIndex + photoAlbum.length : newIndex;
+      return newIndex < 0 ? (photoAlbum.length % 2 === 0 ? photoAlbum.length - 2 : photoAlbum.length - 1) : newIndex;
     });
   };
+
+  const getPhotosToDisplay = () => {
+    const photosToDisplay = [];
+    for (let i = 0; i < 2; i++) {
+      const index = (currentPhotoIndex + i) % photoAlbum.length;
+      photosToDisplay.push(photoAlbum[index]);
+    }
+    return photosToDisplay;
+  };
+
+  const photosToDisplay = getPhotosToDisplay();
+
   return (
     <div className="feature-container">
       <button className="close-btn" onClick={() => setShowMemoryLane(false)}><BsXLg /></button>
       <button onClick={prevPage}><FaLongArrowAltLeft color={"gold"} size={100} /> <span>Back</span></button>
       <div className="memory-lane">
         <div className="photo-album">
-          {photoAlbum.map((photo, index) => (
-            (index >= currentPhotoIndex && index < currentPhotoIndex + 2) && (
+          {photoAlbum.length === 0 ? (
+            <div className='album-page'>
+              <div className='upload-prompt'>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={(e) => handlePhotoUpload(e.target.files[0])}
+                />
+              </div>
+            </div>
+          ) : (
+            photosToDisplay.map((photo, index) => (
               <div className='album-page' key={photo.id}>
                 <div className='photo'>
                   <img
@@ -38,16 +61,13 @@ const MemoryLane = ({ setShowMemoryLane, handlePhotoClick, photoAlbum, }) => {
                   <p>{photo.caption}</p>
                 </div>
               </div>
-            )
-          ))}
+            ))
+          )}
         </div>
-
       </div>
-
       <button onClick={nextPage}><FaLongArrowAltRight color={"gold"} size={100} /> <span>Next</span></button>
     </div>
   );
 };
-
 
 export default MemoryLane;
