ac2c72dadd10f4fd08f80c0acee6b424b876543a
mrjoemiller <joe@joemiller.co>
2024-05-21 20:05:17 -0400

    photoalbum can handle odd numbers of photos now

--- src/views/ReminiscenceRoom/MemoryLane.js
+++ src/views/ReminiscenceRoom/MemoryLane.js
@@ -4,66 +4,79 @@ import { FaLongArrowAltLeft } from "react-icons/fa";
 import { BsXLg } from "react-icons/bs";
 
 const MemoryLane = ({ setShowMemoryLane, handlePhotoClick, handlePhotoUpload, photoAlbum }) => {
-  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+  const [currentPageIndex, setCurrentPageIndex] = useState(0);
 
   const nextPage = () => {
-    setCurrentPhotoIndex((prevIndex) => {
-      const newIndex = prevIndex + 2;
-      return newIndex >= photoAlbum.length ? 0 : newIndex;
+    setCurrentPageIndex((prevIndex) => {
+      const newIndex = prevIndex + 1;
+      return newIndex >= albumPages.length ? 0 : newIndex;
     });
   };
 
   const prevPage = () => {
-    setCurrentPhotoIndex((prevIndex) => {
-      const newIndex = prevIndex - 2;
-      return newIndex < 0 ? (photoAlbum.length % 2 === 0 ? photoAlbum.length - 2 : photoAlbum.length - 1) : newIndex;
+    setCurrentPageIndex((prevIndex) => {
+      const newIndex = prevIndex - 1;
+      return newIndex < 0 ? albumPages.length - 1 : newIndex;
     });
   };
 
-  const getPhotosToDisplay = () => {
-    const photosToDisplay = [];
-    for (let i = 0; i < 2; i++) {
-      const index = (currentPhotoIndex + i) % photoAlbum.length;
-      photosToDisplay.push(photoAlbum[index]);
+  const albumPageCreator = (photos) => {
+    const pages = [];
+    const photosWithPlaceholder = photos.length % 2 === 0 ? photos : [...photos, { ...photos[photos.length - 1], id: 'placeholder' }];
+
+    for (let i = 0; i < photosWithPlaceholder.length; i += 2) {
+      pages.push(
+        <div className="photo-album" key={`page-${i}`}>
+          <div className='album-page'>
+            <div className='photo'>
+              <img
+                src={photosWithPlaceholder[i].src}
+                alt={photosWithPlaceholder[i].caption}
+                onClick={() => handlePhotoClick(photosWithPlaceholder[i])}
+              />
+              <div className='caption'>
+                <p>{photosWithPlaceholder[i].caption}</p>
+              </div>
+            </div>
+          </div>
+          <div className='album-page'>
+            <div className='photo' style={{ visibility: photosWithPlaceholder[i + 1].id === 'placeholder' ? 'hidden' : 'visible' }}>
+              <img
+                src={photosWithPlaceholder[i + 1].src}
+                alt={photosWithPlaceholder[i + 1].caption}
+                onClick={() => handlePhotoClick(photosWithPlaceholder[i + 1])}
+              />
+              <div className='caption'>
+                <p>{photosWithPlaceholder[i + 1].caption}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
     }
-    return photosToDisplay;
+    return pages;
   };
 
-  const photosToDisplay = getPhotosToDisplay();
+  const albumPages = albumPageCreator(photoAlbum);
 
   return (
     <div className="feature-container">
       <button className="close-btn" onClick={() => setShowMemoryLane(false)}><BsXLg /></button>
       <button onClick={prevPage}><FaLongArrowAltLeft color={"gold"} size={100} /> <span>Back</span></button>
       <div className="memory-lane">
-        <div className="photo-album">
-          {photoAlbum.length === 0 ? (
-            <div className='album-page'>
-              <div className='upload-prompt'>
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={(e) => handlePhotoUpload(e.target.files[0])}
-                />
-              </div>
+        {photoAlbum.length === 0 ? (
+          <div className='album-page'>
+            <div className='upload-prompt'>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={(e) => handlePhotoUpload(e.target.files[0])}
+              />
             </div>
-          ) : (
-            photosToDisplay.map((photo, index) => (
-              <div className='album-page' key={photo.id}>
-                <div className='photo'>
-                  <img
-                    src={photo.src}
-                    alt={photo.caption}
-                    onClick={() => handlePhotoClick(photo)}
-                  />
-                </div>
-                <div className='caption'>
-                  <p>{photo.caption}</p>
-                </div>
-              </div>
-            ))
-          )}
-        </div>
+          </div>
+        ) : (
+          albumPages[currentPageIndex]
+        )}
       </div>
       <button onClick={nextPage}><FaLongArrowAltRight color={"gold"} size={100} /> <span>Next</span></button>
     </div>
