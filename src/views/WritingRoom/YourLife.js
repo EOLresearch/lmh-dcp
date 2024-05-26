dfb0c80324309d4b2ce4cdc9b2699ad83695dc26
mrjoemiller <joe@joemiller.co>
2024-05-26 11:10:56 -0400

    writing room

--- src/views/WritingRoom/YourLife.js
+++ src/views/WritingRoom/YourLife.js
@@ -15,9 +15,10 @@ const YourLife = ({ setShowYourLife, handlePhotoClick, photoAlbum }) => {
           <button onClick={() => setOpenLifeBook(false)} id="book-close-button"><FaLongArrowAltLeft color={"gold"} size={100}/> <span>Close</span></button>
           <div className='life-book-copy-container'>
             <div className='life-book-left-side'>
-                
+                sdsdf
             </div>
             <div className='life-book-right-side'>
+              sdfdsf
             </div>
           </div>
 
@@ -25,7 +26,7 @@ const YourLife = ({ setShowYourLife, handlePhotoClick, photoAlbum }) => {
       ) : (
         <>
           <button style={{ visibility: 'hidden' }} ><FaLongArrowAltLeft color={"gold"} size={100} /> <span>Close</span></button>
-          <div className="your-life" onClick={() => setOpenLifeBook(true)}>
+          <div className="book-cover" onClick={() => setOpenLifeBook(true)}>
           </div>
           <button onClick={() => setOpenLifeBook(true)}><FaLongArrowAltRight color={"gold"} size={100} /> <span>Open</span></button>
         </>
