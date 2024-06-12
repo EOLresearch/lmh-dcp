df88aa5be915dfdf3d258f20fe08c6e04f0970fb
mrjoemiller <joe@joemiller.co>
2024-06-12 09:49:39 -0400

    customtoolbar for quill in for the left side of the page

---
+++ src/views/WritingRoom/YourLife/CustomToolBar.js
@@ -0,0 +1,64 @@
+const CustomToolbar = () => (
+  <div id="toolbar">
+    <span className="ql-formats">
+      <button className="ql-bold"></button>
+      <button className="ql-italic"></button>
+      <button className="ql-underline"></button>
+      <button className="ql-strike"></button>
+    </span>
+    <span className="ql-formats">
+      <button className="ql-header" value="1"></button>
+      <button className="ql-header" value="2"></button>
+    </span>
+    <span className="ql-formats">
+      <button className="ql-list" value="ordered"></button>
+      <button className="ql-list" value="bullet"></button>
+    </span>
+    <span className="ql-formats">
+      <button className="ql-script" value="sub"></button>
+      <button className="ql-script" value="super"></button>
+    </span>
+    <span className="ql-formats">
+      <button className="ql-indent" value="-1"></button>
+      <button className="ql-indent" value="+1"></button>
+    </span>
+    <span className="ql-formats">
+      <select className="ql-size">
+        <option value="small"></option>
+        <option selected></option>
+        <option value="large"></option>
+        <option value="huge"></option>
+      </select>
+    </span>
+    <span className="ql-formats">
+      <select className="ql-header">
+        <option value="1"></option>
+        <option value="2"></option>
+        <option value="3"></option>
+        <option value="4"></option>
+        <option value="5"></option>
+        <option value="6"></option>
+        <option selected></option>
+      </select>
+    </span>
+    <span className="ql-formats">
+      <select className="ql-color"></select>
+      <select className="ql-background"></select>
+    </span>
+    <span className="ql-formats">
+      <select className="ql-font"></select>
+    </span>
+    <span className="ql-formats">
+      <select className="ql-align"></select>
+    </span>
+    <span className="ql-formats">
+      <button className="ql-clean"></button>
+    </span>
+    <span className="ql-formats">
+      <button className="ql-link"></button>
+      <button className="ql-image"></button>
+    </span>
+  </div>
+);
+
+export default CustomToolbar;
--- src/views/WritingRoom/YourLife/LifeBookContent.js
+++ src/views/WritingRoom/YourLife/LifeBookContent.js
@@ -3,6 +3,7 @@ import { FaLongArrowAltLeft } from "react-icons/fa";
 import PromptIndex from '../PromptIndex';
 import PromptView from '../PromptView';
 import ReactQuill from 'react-quill';
+import CustomToolbar from './CustomToolBar';
 import 'react-quill/dist/quill.snow.css';
 import { useAuth } from '../../../auth/AuthContext';
 import axios from 'axios';
@@ -41,7 +42,7 @@ const LifeBookContent = ({
     if (file) {
       const formData = new FormData();
       formData.append('file', file);
-  
+
       // Mocking the API call
       setTimeout(() => {
 
@@ -54,7 +55,32 @@ const LifeBookContent = ({
       }, 1000); // Simulating network latency
     }
   };
-  
+
+
+  // const toolbarOptions = [
+  //   ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+  //   [{ 'header': 1 }, { 'header': 2 }], // custom button values
+  //   [{ 'list': 'ordered' }, { 'list': 'bullet' }], // lists
+  //   [{ 'script': 'sub' }, { 'script': 'super' }], // superscript/subscript
+  //   [{ 'indent': '-1' }, { 'indent': '+1' }], // outdent/indent
+  //   [{ 'size': ['small', false, 'large', 'huge'] }], // custom dropdown
+  //   [{ 'header': [1, 2, 3, 4, 5, 6, false] }], // header levels
+  //   [{ 'color': [] }, { 'background': [] }], // dropdown with defaults from theme
+  //   [{ 'font': [] }], // font families
+  //   [{ 'align': [] }], // text align
+  //   ['clean'], // remove formatting
+  //   ['link', 'image'] // links, images, video
+  // ];
+
+  // const modules = {
+  //   toolbar: toolbarOptions
+  // };
+
+  // Define the formats you want to allow in your editor
+  const formats = [
+    'header', 'font', 'size', 'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent', 'link', 'image', 'color', 'background', 'align'
+  ];
 
   return (
     <div className="open-life-book-content">
@@ -69,14 +95,15 @@ const LifeBookContent = ({
             />
           ) : (
             showWelcome ? (
-              <div>
+              <>
                 <h3>Welcome to the "This is Your Life" feature!</h3>
                 <p>This tool allows you to document and celebrate the life story of the person you are caring for. By interviewing them and using structured prompts, you will create a comprehensive and cherished biography that highlights major milestones and experiences.</p>
-              </div>
+              </>
             ) : (
-              <div>
+              <>
                 <h2>{allPrompts[currentPromptIndex]}</h2>
-                <button onClick={() => imageInputRef.current.click()} className="upload-button">Upload Image</button>
+                <CustomToolbar />
+                {/* <button onClick={() => imageInputRef.current.click()} className="upload-button">Upload Image</button>
                 <input
                   type="file"
                   ref={imageInputRef}
@@ -84,29 +111,33 @@ const LifeBookContent = ({
                   accept="image/*"
                   onChange={handleImageChange}
                 />
-                {uploadedImage && <img src={uploadedImage} alt="Uploaded" className="uploaded-thumbnail" />}
-              </div>
+                {uploadedImage && <img src={uploadedImage} alt="Uploaded" className="uploaded-thumbnail" />} */}
+              </>
             )
           )}
         </div>
         <div className='life-book-right-side'>
           {showWelcome ? (
-            <div>
+            <>
               <h3>On the next page you will be met with the first prompt for your Life Book.</h3>
               <p>We encourage you to upload photos where suitable to help illustrate your story.</p>
               <button onClick={handleNextPrompt} className="next-button">Next</button>
-            </div>
+            </>
           ) : (
-            <div>
+            <div className='quill-container'>
               <ReactQuill
                 value={editorContent}
                 onChange={handleEditorChange}
-                theme="snow"
+                modules={{ toolbar: { container: "#toolbar" } }}
+                formats={formats}
               />
-              <button onClick={handleSave} className="save-button">Save</button>
-              {currentPromptIndex > 0 && <button onClick={handlePreviousPrompt} className="back-button">Previous</button>}
-              {currentPromptIndex < allPrompts.length - 1 && <button onClick={handleNextPrompt} className="next-button">Next</button>}
-              <button onClick={handleViewIndex} className="view-index-button">View Index</button>
+
+              <div className="prompts-toolbar">
+                <button onClick={handleSave} className="save-button">Save</button>
+                {currentPromptIndex > 0 && <button onClick={handlePreviousPrompt} className="back-button">Previous</button>}
+                {currentPromptIndex < allPrompts.length - 1 && <button onClick={handleNextPrompt} className="next-button">Next</button>}
+                <button onClick={handleViewIndex} className="view-index-button">View Index</button>
+              </div>
             </div>
           )}
         </div>
